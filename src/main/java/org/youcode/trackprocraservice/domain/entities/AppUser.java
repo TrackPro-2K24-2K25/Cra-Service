ab9bfeca37bd80f277523c001df1fea6383c5c5a
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-02 19:02:20 +0100

    Refactor AppUser with embeddable classes for user details

--- src/main/java/org/youcode/trackprocraservice/domain/entities/AppUser.java
+++ src/main/java/org/youcode/trackprocraservice/domain/entities/AppUser.java
@@ -5,8 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.youcode.trackprocraservice.domain.enums.AccountStatus;
 import org.youcode.trackprocraservice.domain.enums.Role;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -14,18 +19,72 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class AppUser {
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
+    @Column(unique = true)
     private String username;
+    private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Embedded
+    private ContactInfo contactInfo;
 
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private ProfileInfo profileInfo;
+
+    @Embedded
+    private Preferences preferences;
+
+    @Embedded
+    private SecurityInfo securityInfo;
+
+    @Embedded
+    private AuditInfo auditInfo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.securityInfo.isLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.preferences.getAccountStatus() == AccountStatus.ACTIVE;
+    }
 }
