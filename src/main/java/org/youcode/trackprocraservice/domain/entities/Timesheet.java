38bb4a20e47968fcdd5f146b243e8508b8493e6f
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-02 14:43:17 +0100

    add timesheet entity

--- src/main/java/org/youcode/trackprocraservice/domain/entities/Timesheet.java
+++ src/main/java/org/youcode/trackprocraservice/domain/entities/Timesheet.java
@@ -1,5 +1,46 @@
 package org.youcode.trackprocraservice.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.youcode.trackprocraservice.domain.enums.Status;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 public class Timesheet {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ElementCollection
+    private List<Comments> comments;
+
+    @ElementCollection
+    private List<WorkPeriod> selectedPeriods;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date date;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Getter
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "timesheet_fichiers",
+            joinColumns = { @JoinColumn(name = "timesheet_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "file_id", referencedColumnName = "id")}
+    )
+    private List<File> files = new ArrayList<>();
+
+
+
+
+
+
+
 }
