dd1a9f91f2b73a4f36115556ed308dafbc1f6e2b
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-06 08:54:05 +0100

    add PaymentTermRepository

---
+++ src/main/java/org/youcode/trackprocraservice/repository/interfaces/PaymentTermRepository.java
@@ -0,0 +1,11 @@
+package org.youcode.trackprocraservice.repository.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.youcode.trackprocraservice.domain.entities.PaymentTerm;
+
+import java.util.UUID;
+
+@Repository
+public interface PaymentTermRepository extends JpaRepository<PaymentTerm, UUID> {
+}
