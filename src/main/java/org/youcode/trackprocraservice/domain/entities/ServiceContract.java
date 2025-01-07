199d67d5840607521c1494410ce1496a44496b92
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-07 18:19:53 +0100

    update entity

--- src/main/java/org/youcode/trackprocraservice/domain/entities/ServiceContract.java
+++ src/main/java/org/youcode/trackprocraservice/domain/entities/ServiceContract.java
@@ -19,6 +19,7 @@ public class ServiceContract {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(unique = true, nullable = false)
     private String name;
     private String description;
 
