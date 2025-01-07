3503f6cbd47683752c889e18e7ed8d7c337d02ac
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-07 18:56:15 +0100

    add relation between InvoicingConditions and PaymentTerm

--- src/main/java/org/youcode/trackprocraservice/domain/entities/PaymentTerm.java
+++ src/main/java/org/youcode/trackprocraservice/domain/entities/PaymentTerm.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -27,5 +28,8 @@ public class PaymentTerm {
     private int days;
     private boolean isDefault;
     private boolean isActive;
+
+    @OneToMany(mappedBy = "paymentTerm", fetch = FetchType.LAZY)
+    private List<InvoicingConditions> invoicingConditions;
 
 }
