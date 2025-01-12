ea67a6664366dc94453dc9b06c8bb5d5520301df
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2025-01-12 20:25:44 +0100

    add file exceptions

--- src/main/java/org/youcode/trackprocraservice/web/errors/GlobalExceptionHandler.java
+++ src/main/java/org/youcode/trackprocraservice/web/errors/GlobalExceptionHandler.java
@@ -4,8 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.youcode.trackprocraservice.exception.File.FileNotFoundException;
+import org.youcode.trackprocraservice.exception.File.FileStorageException;
+import org.youcode.trackprocraservice.exception.File.FileValidationException;
 import org.youcode.trackprocraservice.exception.InvoicingCurrency.InvoicingCurrencyNotFoundException;
 import org.youcode.trackprocraservice.exception.InvoicingCurrency.InvoicingCurrencyValidationException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +24,30 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvoicingCurrencyValidationException.class)
     public ResponseEntity<String> handleInvoicingCurrencyValidation(InvoicingCurrencyValidationException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleFileNotFoundException(FileNotFoundException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "File Not Found");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(FileValidationException.class)
+    public ResponseEntity<Map<String, String>> handleFileValidationException(FileValidationException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Validation Error");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public ResponseEntity<Map<String, String>> handleFileStorageException(FileStorageException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "File Storage Error");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
